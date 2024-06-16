bea5fdde4a9d8b82c2cf4234e3c13a852e629211
gocha001 <kalianichgocha@ukr.net>
2024-06-16 19:30:48 +0300

    add initial files

---
+++ js/task-3.js
@@ -0,0 +1,33 @@
+class StringBuilder {
+  #value;
+
+  constructor(initialValue) {
+    this.#value = initialValue;
+  }
+
+  getValue() {
+    return this.#value;
+  }
+
+  padStart(str) {
+    this.#value = this.#value.padStart(this.#value.length + str.length, str);
+  }
+
+  padEnd(str) {
+    this.#value = this.#value.padEnd(this.#value.length + str.length, str);
+  }
+
+  padBoth(str) {
+    this.padStart(str);
+    this.padEnd(str);
+  }
+}
+
+const builder = new StringBuilder(".");
+console.log(builder.getValue()); // "."
+builder.padStart("^");
+console.log(builder.getValue()); // "^."
+builder.padEnd("^");
+console.log(builder.getValue()); // "^.^"
+builder.padBoth("=");
+console.log(builder.getValue()); // "=^.^="
